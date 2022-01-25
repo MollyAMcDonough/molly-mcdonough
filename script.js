3fc252b7b0446dbf366fbb0fa1a9c7e4c8e64d57
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-01-25 13:34:12 -0500

    added anotherPage.html. linked index.html to it and script.js and style.css updated.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const div = document.createElement("div");
+div.textContent = "Button!";
+div.id = "button"
+document.querySelector("body").appendChild(div);
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  input.style.backgroundColor = "#FF69B4";
+  alert('I was clicked!');
+});
